43b9fbfd68c27b31dc8027061f3e71a40d6557f2
athiras26 <athiraniya1122@gmail.com>
2025-08-08 12:29:20 +0530

    Update run_sim.tcl

--- sim/run_sim.tcl
+++ sim/run_sim.tcl
@@ -1,6 +1,6 @@
 #==============================================================================
 # Tosil Systems Pvt Ltd
-# new run tcl 1
+# new run tcl 5
 # run_build.tcl
 #
 # Synthesis, P&R, and bitfile generation script.
